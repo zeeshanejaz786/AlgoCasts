274a70fada3ab5298e93737cf92cd3dc82da184d
TOMMY-QURESHI <qureshitricky@gmail.com>
2022-09-05 23:41:25 +0500

    queue and stacks implemented and solved questions

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,34 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stack1 = new Stack();
+        this.stack2 = new Stack();
+    }
+    add(record) {
+        this.stack1.push(record);
+    }
+    remove() {
+        while (this.stack1.peek()) {
+            this.stack2.push(this.stack1.pop());
+        }
+        let record = this.stack2.pop();
+        while (this.stack2.peek()) {
+            this.stack1.push(this.stack2.pop());
+        }
+        return record;
+    }
+    peek() {
+        while (this.stack1.peek()) {
+            this.stack2.push(this.stack1.pop());
+        }
+        let record = this.stack2.peek();
+        while (this.stack2.peek()) {
+            this.stack1.push(this.stack2.pop());
+        }
+        return record;
+    }
+}
 
 module.exports = Queue;
--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -19,6 +19,10 @@ class Queue {
     remove() {
         return this.data.pop();
     }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+    
 }
 
 module.exports = Queue;
